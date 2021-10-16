0321371352816a67e9dfafa01d6aa259633f2b47
aparaxium <10635434+Aparaxium@users.noreply.github.com>
2021-10-16 00:23:06 -0700

    Add custom 404 page, change pack.json name

---
+++ pages/404.tsx
@@ -0,0 +1,7 @@
+export default function Custom404() {
+  return (
+    <h1 className="h-screen w-full flex justify-center items-center text-8xl font-bold">
+      404 - Page Not Found
+    </h1>
+  );
+}
